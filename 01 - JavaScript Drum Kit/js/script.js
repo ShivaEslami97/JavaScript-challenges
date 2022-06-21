0d580cf2cee05e9261858eebce0c46bc2f287a3f
ShivaEslami97 <shivaslami@gmail.com>
2022-06-21 16:07:26 +0430

    Update play audio function

--- 01 - JavaScript Drum Kit/js/script.js
+++ 01 - JavaScript Drum Kit/js/script.js
@@ -5,18 +5,25 @@ const key = document.querySelectorAll('.key[data-key]');
 const audio = document.querySelectorAll('.audio[data-key]');
 
 const playAudio = (e) => {
+    /// KeyboardEvent.keyCode been deprecated for some time. Use KeyboardEvent.code.
     const keyNum = (e.type == 'click') ? e.currentTarget.dataset.key : e.code;
-    const audioSelected = document.querySelector(`.audio[data-key="${keyNum}"]`);
     const keySelected = document.querySelector(`.key[data-key="${keyNum}"]`);
-    keySelected.classList.add('playing');
-    if (audioSelected) {
+    if (keySelected) {
+        const audioSelected = keySelected.querySelector('audio');
+        keySelected.classList.add('playing');
+        audioSelected.currentTime = 0;  //rewind to the start
         audioSelected.play();
-    }
-    audioSelected.currentTime = 0;
+    };
 }
 
 key.forEach(element => {
+    /// click => play sound
     element.addEventListener('click', playAudio);
+    /// after transition end => remove playing class
+    element.addEventListener('transitionend', (e) => {
+        console.log(e);
+        element.classList.remove("playing");
+    });
 });
 
 window.addEventListener('keydown', playAudio);
