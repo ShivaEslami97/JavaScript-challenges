c773b56ab7724cb3c1b42e5a284d91497a2823d7
ShivaEslami97 <shivaslami@gmail.com>
2022-06-29 00:27:10 +0430

    Initial commit

---
+++ 02 - Dad-Jokes/js/script.js
@@ -0,0 +1,37 @@
+'use strict';
+const joke = document.querySelector('.joke__text');
+const jokeBtn = document.querySelector('.joke__btn-joke');
+const twitterBtn = document.querySelector('.joke__btn-twitter');
+const loading = document.querySelector('.lds-ellipsis');
+const jokeBtns = document.querySelector('.joke__bottom');
+
+function getJoke() {
+    const options = {
+        headers: {
+            Accept: 'application/json',
+        }
+    }
+    joke.textContent = '';
+    joke.classList.remove('in');
+    loading.classList.remove('hidden');
+    jokeBtns.classList.add('hidden');
+    //// fetch
+    fetch('https://icanhazdadjoke.com/', options)
+        .then(response => response.json())
+        .then(data => {
+            setTimeout(function () {
+                joke.classList.add('in');
+                joke.textContent = data.joke;
+            }, 500);
+            loading.classList.add('hidden');
+            jokeBtns.classList.remove('hidden');
+        });
+}
+function tweetQuote() {
+    const tweet = joke.innerHTML;
+    if (tweet.trim() !== '')
+        window.open(`https://twitter.com/home/?text=${tweet}`);
+}
+getJoke();
+jokeBtn.addEventListener('click', getJoke);
+twitterBtn.addEventListener('click', tweetQuote);
